8773e359181ea1a9088762a1a8277a38ae7dce75
xonder05 <daniel.onderk@gmail.com>
2026-02-12 18:53:39 +0100

    Add: ros-interface node, this node handles starting / stopping interface and presents interfaces state to user Update: publisher and subscriber nodes updated to work with changes to interface, this is significant simplification since they don't have to manually register commands topics and similar, also dropped node specific endpoints Change: small change to State class, mostly to align the api with js conventions

---
+++ nodes/interface/ros-interface.js
@@ -0,0 +1,98 @@
+/**
+ * @file ros-interface.js
+ * @brief This node commands ROS2 interface and presents its state to user.
+ * @author Daniel Onderka (xonder05)
+ * @date 01/2026
+ */
+
+module.exports = function(RED) 
+{
+    const ros2 = require("../../ros2_interface_api/ros2_interface_api.js");
+
+    function ROSInterface(config) 
+    {
+        RED.nodes.createNode(this, config);
+        const node = this;
+
+        function init()
+        {
+            if (config.start == "deploy") {
+                RED.events.once("flows:started", ros2.launch);
+            }
+
+            ros2.state.on("change", interface_state_change_handler);
+
+            node.on("input", input_event_handler);
+            node.on("close", close_event_handler);
+        }
+
+        async function interface_state_change_handler(state)
+        {
+            if (state == "inactive")
+            {
+                node.state = {fill: "grey", shape: "ring", text: "Offline"};
+                node.status(node.state);
+            }
+            else
+            {
+                const service_name = "commands";
+                const message_type = "node_manager/DictionarySerialized";
+                const qos = {
+                    history: {
+                        kind: "KEEP_LAST",
+                        depth: 10
+                    },
+                    reliability: "RELIABLE",
+                    durability: "VOLATILE"
+                };
+
+                try 
+                {
+                    node.client = await ros2.consume_service(node.id, service_name, message_type, qos)
+                    
+                    node.state = {fill: "green", shape: "dot", text: "Online"};
+                    node.status(node.state);
+                }
+                catch (error) 
+                {
+                    node.state = {fill: "yellow", shape: "dot", text: "Online, no manager service"};
+                    node.status(node.state);
+                }
+            }
+        }
+
+        function input_event_handler(msg, send, done)
+        {
+            if (msg.payload == "start") {
+                if (ros2.state.get() == "inactive") {
+                    ros2.launch();
+                }
+            }
+            
+            if (msg.payload == "stop") {
+                if (ros2.state.get() == "active") {
+                    ros2.stop();
+                }
+            }
+
+            done();
+        }
+
+        async function close_event_handler(removed, done)
+        {
+            if (["green", "yellow"].includes(node.state.fill))
+            {
+                ros2.stop();
+            }
+
+            if (["grey", "red"].includes(node.state.fill))
+            {
+                done();
+            }
+        }
+
+        init();
+    };
+
+    RED.nodes.registerType("ros-interface", ROSInterface);
+}
--- nodes/ros-subscriber/ros-subscriber.js
+++ nodes/ros-subscriber/ros-subscriber.js
@@ -1,14 +1,15 @@
 /**
  * @file ros-subscriber.js
- * @author Daniel Onderka
- * @date 10/2025
+ * @author Daniel Onderka (xonder05)
+ * @date 10/2025, 01/2026
  */
 
 module.exports = function(RED) 
 {
-    var is_web_api = require("is-web-api");
+    const ros2 = require("../../ros2_interface_api/ros2_interface_api.js");
 
-    function ROSSubscriber(config) {
+    function ROSSubscriber(config) 
+    {
         RED.nodes.createNode(this,config);
         const node = this;
 
@@ -16,99 +17,41 @@ module.exports = function(RED)
         {
             // slice only the node id without subflows
             node.node_id = node.id.slice(node.id.lastIndexOf("-") + 1)
-
-            node.state = {fill: "grey", shape: "dot", text: "Offline"};
-            node.status(node.state);
-
-            register_is();
-
-            RED.events.once("flows:started", flow_started_event_handler.bind(this))
-            node.on("close", close_event_handler.bind(this));
-
             node.log = "";
-            RED.httpAdmin.get("/ros-subscriber/get-log", RED.auth.needsPermission("ros-subscriber.read"), function (req, res) 
+
+            ros2.state.on("change", interface_state_change_handler);
+
+            node.on("close", close_event_handler);
+        }
+
+        async function interface_state_change_handler(state)
+        {
+            if (state == "inactive")
             {
-                res.send(node.log);
-            });
-        }
-
-        function flow_started_event_handler()
-        {
-            is_web_api.launch();
-        }
-
-        function close_event_handler(removed, done)
-        {
-            unregister_is();
-
-            is_web_api.launch();
-
-            node.state = {fill: "yellow", shape: "dot", text: "Waiting for Integration Service"}
-            node.status(node.state);
-
-            done();
-        }
-
-        function register_is()
-        {
-            // join commands topic
-            const interface_path1 = get_interface_path("node_manager", "NodeRedCommand");
-            const folder_path1 = interface_path1.slice(0, interface_path1.lastIndexOf("/"));
-            is_web_api.add_custom_ros2_type("node_manager", "NodeRedCommand", folder_path1);
-
-            const topic_name = "management/commands";
-            const message_type = "node_manager/NodeRedCommand";
-            const qos = {
-                history: {
-                    kind: "KEEP_LAST",
-                    depth: 10
-                },
-                reliability: "RELIABLE",
-                durability: "VOLATILE"
-            };
-
-            is_web_api.add_publisher(topic_name, message_type, qos);
-            is_web_api.add_subscriber(topic_name, message_type, qos);
-
-
-            // add type
-            const interface_name = config.interface.slice(config.interface.lastIndexOf("/") + 1);
-            const interface_path = get_interface_path(config.package, interface_name);
-            const folder_path = interface_path.slice(0, interface_path.lastIndexOf("/"));
-            // is_web_api.add_custom_ros2_type(config.package, interface_name, folder_path);
-            
-
-            is_web_api.add_ros2_type(config.package, interface_name);
-
-            // subsribe
-            is_web_api.add_subscriber(config.topic, config.package + "/" + interface_name, []);
-            node.log = "";
-
-            const event_emitter = is_web_api.get_event_emitter();
-
-            event_emitter.on(config.topic, log_callback);
-        }
-
-        function unregister_is()
-        {
-            // add type
-            const interface_name = config.interface.slice(config.interface.lastIndexOf("/") + 1);
-            const interface_path = get_interface_path(config.package, interface_name);
-            const folder_path = interface_path.slice(0, interface_path.lastIndexOf("/"));
-            // is_web_api.remove_custom_ros2_type(config.package, interface_name, folder_path);
-            is_web_api.remove_ros2_type(config.package, interface_name);
-
-            // subsribe
-            is_web_api.remove_subscriber(config.topic);
-
-            const event_emitter = is_web_api.get_event_emitter();
-
-            event_emitter.off(config.topic, log_callback);
+                node.state = {fill: "grey", shape: "ring", text: "Interface offline"};
+                node.status(node.state);
+            }
+            else
+            {
+                try
+                {
+                    [_, type] = config.interface.split("/");
+                    node.subscriber = await ros2.subscribe_topic(node.id, config.topic, config.package + "/" + type, log_callback);
+                
+                    node.state = {fill: "green", shape: "dot", text: "Subscribed"} 
+                    node.status(node.state);
+                } 
+                catch (error) 
+                {
+                    node.state = {fill: "red", shape: "dot", text: "Error, failed to subscribe"} 
+                    node.status(node.state);   
+                }
+            }
         }
 
         function log_callback(msg)
         {
-            node.log = node.log + msg.msg?.data + "\n";
+            node.log = node.log + msg.payload?.data + "\n";
 
             RED.comms.publish("log", {
                 id: node.id,
@@ -116,80 +59,35 @@ module.exports = function(RED)
             }, true);
 
             out = {
-                payload: msg.msg?.data,
+                payload: msg.payload?.data,
             }
 
             node.send(out)
+        }
+
+        async function close_event_handler(removed, done)
+        {
+            if (["green"].includes(node.state.fill)) 
+            {
+                try 
+                {
+                    await node.subscriber.destroy(node.id);
+
+                    node.state = {fill: "grey", shape: "dot", text: "Unsubscribed"};
+                    node.status(node.state);
+                } 
+                catch (error) {
+                    // todo handle
+                }
+            }
+
+            if (["grey", "red"].includes(node.state.fill)) {
+                done();
+            }
         }
 
         init();
     }
 
     RED.nodes.registerType("ros-subscriber", ROSSubscriber);
-
-//-------------------- Instance independent Endpoints and Helpers --------------------
-
-    RED.httpAdmin.get("/ros-subscriber/list-packages", RED.auth.needsPermission("ros-subscriber.read"), function (req, res) 
-    {
-        const { exec } = require("child_process");
-        const cmd = `python3 -c "import json; from ament_index_python.packages import get_packages_with_prefixes; print(json.dumps(list(get_packages_with_prefixes().keys())))"`;
-
-        exec(cmd, (error, stdout, stderr) => 
-        {
-            if (error) {
-                console.error(`Exec error: ${error.message}`);
-                return res.status(500).json({ error: "Python execution failed" });
-            }
-
-            try {
-                const packages = JSON.parse(stdout);
-                res.json(packages);
-            }
-            catch (err) {
-                console.error("JSON parse error:", err);
-                res.status(500).json({ error: "Invalid JSON output" });
-            }
-        });
-    });
-
-    RED.httpAdmin.get("/ros-subscriber/list-interfaces", RED.auth.needsPermission("ros-subscriber.read"), function (req, res) 
-    {
-        const selectedPackage = req.query.package;
-
-        const { exec } = require("child_process");
-        const cmd = `python3 -c "import json; from rosidl_runtime_py import get_message_interfaces; print(json.dumps(get_message_interfaces(['${selectedPackage}'])['${selectedPackage}']))"`;
-
-        exec(cmd, (error, stdout, stderr) => 
-        {
-            if (error) {
-                console.error(`Exec error: ${error.message}`);
-                return res.status(500).json({ error: "Python execution failed" });
-            }
-
-            try {
-                const packages = JSON.parse(stdout);
-                res.json(packages);
-            } 
-            catch (err) {
-                console.error("JSON parse error:", err);
-                res.status(500).json({ error: "Invalid JSON output" });
-            }
-        });
-    });
-
-    RED.httpAdmin.get("/ros-subscriber/get-log", RED.auth.needsPermission("ros-subscriber.read"), function (req, res) 
-    {
-        res.send(node.log);
-    });
-
-    function get_interface_path(package, interface)
-    {
-        const { execSync } = require("child_process");
-
-        const package_name = package + "/msg/" + interface;
-        const cmd = `python3 -c "from rosidl_runtime_py import get_interface_path; print(get_interface_path('${package_name}'))"`;
-
-        const stdout = execSync(cmd, {encoding: "utf8"}); 
-        return stdout;
-    }
 }
